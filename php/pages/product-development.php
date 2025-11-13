6bd2353f01838b0064b65bc1d114a9dcb02a0b9a
AksharaKM <akshara.spaceylon@gmail.com>
2025-11-13 15:23:47 +0530

    optimize js updated

--- php/pages/product-development.php
+++ php/pages/product-development.php
@@ -1,30 +1,38 @@
 <link rel="stylesheet" href="css/content_style.css">
 <div class="page-section-holder" id="forcast-chart">
-        <h3>Sales Analyst  For Next 6 Months<span class="blinking-indicator"></span></h3>
+        <h3>Box Specification<span class="blinking-indicator"></span></h3>
             <canvas id="salesChart"></canvas>
     </div>
 
     <div class="page-section-holder" id="forcast-chart">
-        <h3>Sales Analyst  For Next 6 Months<span class="blinking-indicator"></span></h3>
+        <h3>Insert Specification<span class="blinking-indicator"></span></h3>
             <canvas id="salesChart"></canvas>
     </div>
     <div class="page-section-holder" id="forcast-chart">
-        <h3>Sales Analyst  For Next 6 Months<span class="blinking-indicator"></span></h3>
+        <h3>Insert Holder Specification<span class="blinking-indicator"></span></h3>
             <canvas id="salesChart"></canvas>
     </div>
     <div class="page-section-holder" id="forcast-chart">
-        <h3>Sales Analyst  For Next 6 Months<span class="blinking-indicator"></span></h3>
+        <h3>Insert Holder Platform Specification<span class="blinking-indicator"></span></h3>
             <canvas id="salesChart"></canvas>
     </div>
     <div class="page-section-holder" id="forcast-chart">
-        <h3>Sales Analyst  For Next 6 Months<span class="blinking-indicator"></span></h3>
+        <h3>Insert Partiton Specification<span class="blinking-indicator"></span></h3>
             <canvas id="salesChart"></canvas>
     </div>
     <div class="page-section-holder" id="forcast-chart">
-        <h3>Sales Analyst  For Next 6 Months<span class="blinking-indicator"></span></h3>
+        <h3>Lid Specification<span class="blinking-indicator"></span></h3>
             <canvas id="salesChart"></canvas>
     </div>
     <div class="page-section-holder" id="forcast-chart">
-        <h3>Sales Analyst  For Next 6 Months<span class="blinking-indicator"></span></h3>
+        <h3>Base Specification<span class="blinking-indicator"></span></h3>
+            <canvas id="salesChart"></canvas>
+    </div>
+     <div class="page-section-holder" id="forcast-chart">
+        <h3>Platform Specification<span class="blinking-indicator"></span></h3>
+            <canvas id="salesChart"></canvas>
+    </div>
+     <div class="page-section-holder" id="forcast-chart">
+        <h3>Sleeve Specification<span class="blinking-indicator"></span></h3>
             <canvas id="salesChart"></canvas>
     </div>
